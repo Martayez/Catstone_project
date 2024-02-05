36f8b2ff146490f60fef9e3d03935c08dab551a4
martayez <martayezprogramming@gmail.com>
2024-02-04 23:09:47 -0600

    picture and text

--- index.js
+++ index.js
@@ -7,17 +7,16 @@ import { type } from "os";
 const app = express();
 const port = 3000;
 
-app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
     try{
         const response_fact = await axios.get("https://catfact.ninja/fact");
-        console.log(response_fact.data.fact);
+        // console.log(response_fact.data.fact);
         const response_pic = await axios.get("https://api.thecatapi.com/v1/images/search");
         // console.log(Object.keys(response_pic));
-        console.log(response_pic.data[0].url);
+        // console.log(response_pic.data[0].url);
         res.render("index.ejs", {
             fact: response_fact.data.fact,
             picture: response_pic.data[0].url
